4c1e6acc930288b1c1cef1be8ff8589ce336da51
luis.villar <luis.villar@next-technologies.com.mx>
2023-09-08 13:05:27 -0600

    sayHello dev

--- dev.js
+++ dev.js
@@ -2,5 +2,7 @@ const sum = (a, b) => a + b;
 
 const factor = (base, factor) => base * factor;
 
+const sayHello = (username, country) = `Hello ${username} from ${country}`;
+
 
 console.log('sum', sum(5, 7));
